39b193f0500391fce06d8969540d980ce04c95e5
Hillihou <Hillihou@gmail.com>
2021-07-16 22:29:21 +0800

    newutil

--- Project01/src/com/iweb/Test/FictonTest.java
+++ Project01/src/com/iweb/Test/FictonTest.java
@@ -3,9 +3,12 @@ package com.iweb.Test;
 import com.iweb.Control.FictionCtl;
 import com.iweb.DAO.FictionDAOimpl;
 import com.iweb.model.Fiction;
+import com.iweb.utils.CMUtility;
 import com.iweb.utils.OtherUtils;
 
+
 import java.util.List;
+import java.util.Scanner;
 
 public class FictonTest {
     public static void main(String[] args) {
@@ -20,12 +23,21 @@ public class FictonTest {
 //        System.out.println(fictionDAOimpl.showfiction(1));
 //
 //        System.out.println(new C_fictionDAOimpl().showC_fiction());
-        FictionDAOimpl fiction = FictionDAOimpl.getInstance();
-        List list = fiction.showAllfiction();
-        OtherUtils.showList(list);
-        Fiction showfiction = fiction.showfiction(1);
-        System.out.println(showfiction);
-        FictionCtl fictionCtl = new FictionCtl();
-        fictionCtl.FicChoose();
+
+
+//        FictionDAOimpl fiction = FictionDAOimpl.getInstance();
+//        List list = fiction.showAllfiction();
+//        OtherUtils.showList(list);
+//        Fiction showfiction = fiction.showfiction(1);
+//        System.out.println(showfiction);
+//        FictionCtl fictionCtl = new FictionCtl();
+//        fictionCtl.FicChoose();
+
+//        String s = CMUtility.readString(3);
+//
+//        int i = CMUtility.readInt(12);
+//        System.out.println(s+"s");
+//        System.out.println(i+"i");
+
     }
 }
